2929853547ebec495f269c8f965c7a36de9f0dcd
Ezra Bradford <ezra.bradford@gmail.com>
2014-06-05 21:35:18 -0700

    Test accessing a removed component
    We shouldn't be able to get components that we've deleted

--- EntitySystemJava/tests/com/wikidot/entitysystems/rdbmswithcodeinsystems/tests/TestKillEntity.java
+++ EntitySystemJava/tests/com/wikidot/entitysystems/rdbmswithcodeinsystems/tests/TestKillEntity.java
@@ -1,9 +1,15 @@
 package com.wikidot.entitysystems.rdbmswithcodeinsystems.tests;
 
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.wikidot.entitysystems.rdbmswithcodeinsystems.EntityManager;
+import com.wikidot.entitysystems.rdbmswithcodeinsystems.examplecomponents.Position;
 
 public class TestKillEntity {
 
@@ -18,7 +24,7 @@ public class TestKillEntity {
 	public void testKillingTwoForwards() {
 		int first = em.createEntity();
 		int second = em.createEntity();
-		
+
 		em.killEntity(first);
 		em.killEntity(second);
 	}
@@ -27,8 +33,33 @@ public class TestKillEntity {
 	public void testKillingTwoBackwards() {
 		int first = em.createEntity();
 		int second = em.createEntity();
-		
+
 		em.killEntity(second);
 		em.killEntity(first);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAccessingRemovedComponentIndividually() {
+		int entity = em.createEntity();
+		Position pos = new Position();
+		pos.x = 5;
+		pos.y = 10;
+		em.addComponent(entity, pos);
+
+		em.killEntity(entity);
+		Position result = em.getComponent(entity, Position.class);
+	}
+	
+	@Test
+	public void testAccessingRemovedComponentList() {
+		int entity = em.createEntity();
+		Position pos = new Position();
+		pos.x = 5;
+		pos.y = 10;
+		em.addComponent(entity, pos);
+
+		em.killEntity(entity);
+		List<Position> result = em.getAllComponentsOfType(Position.class);
+		assertTrue(result.isEmpty());
 	}
 }
