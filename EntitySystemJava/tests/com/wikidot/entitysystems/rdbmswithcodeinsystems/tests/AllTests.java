7a3e261b5178f459d68638d21078b07a7455d967
Ezra Bradford <ezra.bradford@gmail.com>
2014-06-05 18:26:04 -0700

    Add suite to run all tests

---
+++ EntitySystemJava/tests/com/wikidot/entitysystems/rdbmswithcodeinsystems/tests/AllTests.java
@@ -0,0 +1,11 @@
+package com.wikidot.entitysystems.rdbmswithcodeinsystems.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestKillEntity.class, TestPosition.class })
+public class AllTests {
+
+}
